d39602226456d3c09d0350c7ec87c90f78081da8
Jaehwan Lee <jaehwlee@gmail.com>
2021-01-22 20:07:38 +0900

    Update mnist_distributed.py

--- tony-examples/mnist-tensorflow/mnist_distributed.py
+++ tony-examples/mnist-tensorflow/mnist_distributed.py
@@ -293,3 +293,58 @@ def main(_):
 
 if __name__ == '__main__':
     tf.app.run()
+
+    
+    
+'''
+@tf_export(v1=["train.StopAtStepHook"])
+class StopAtStepHook(session_run_hook.SessionRunHook):
+  """Hook that requests stop at a specified step."""
+
+  def __init__(self, num_steps=None, last_step=None):
+    """Initializes a `StopAtStepHook`.
+    This hook requests stop after either a number of steps have been
+    executed or a last step has been reached. Only one of the two options can be
+    specified.
+    if `num_steps` is specified, it indicates the number of steps to execute
+    after `begin()` is called. If instead `last_step` is specified, it
+    indicates the last step we want to execute, as passed to the `after_run()`
+    call.
+    Args:
+      num_steps: Number of steps to execute.
+      last_step: Step after which to stop.
+    Raises:
+      ValueError: If one of the arguments is invalid.
+    """
+    if num_steps is None and last_step is None:
+      raise ValueError("One of num_steps or last_step must be specified.")
+    if num_steps is not None and last_step is not None:
+      raise ValueError("Only one of num_steps or last_step can be specified.")
+    self._num_steps = num_steps
+    self._last_step = last_step
+
+  def begin(self):
+    self._global_step_tensor = training_util._get_or_create_global_step_read()  # pylint: disable=protected-access
+    if self._global_step_tensor is None:
+      raise RuntimeError("Global step should be created to use StopAtStepHook.")
+
+  def after_create_session(self, session, coord):
+    if self._last_step is None:
+      global_step = session.run(self._global_step_tensor)
+      self._last_step = global_step + self._num_steps
+
+  def before_run(self, run_context):  # pylint: disable=unused-argument
+    return SessionRunArgs(self._global_step_tensor)
+
+  def after_run(self, run_context, run_values):
+    global_step = run_values.results + 1
+    if global_step >= self._last_step:
+      # Check latest global step to ensure that the targeted last step is
+      # reached. global_step read tensor is the value of global step
+      # before running the operation. We're not sure whether current session.run
+      # incremented the global_step or not. Here we're checking it.
+
+      step = run_context.session.run(self._global_step_tensor)
+      if step >= self._last_step:
+        run_context.request_stop()
+'''
